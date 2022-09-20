6deeb0b4b626e6d3c5d2ab7ecaba781370f6a17b
Fredrik Åsberg <fredrik@aringinnovation.se>
2022-09-20 22:42:54 +0200

    Exporting imgui headers

--- include/agui/agui.hpp
+++ include/agui/agui.hpp
@@ -3,9 +3,9 @@
 
 #include <GLFW/glfw3.h>
 #include <cstdio>
-#include <imgui/backends/imgui_impl_glfw.h>
-#include <imgui/backends/imgui_impl_opengl3.h>
-#include <imgui/imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <imgui.h>
 #include <iostream>
 #include <string>
 #include <agui/style.hpp>
